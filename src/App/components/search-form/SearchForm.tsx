eaeb33384662686f2400f774ce918bcb004ea802
MedAli5543 <ben.othmen5543@gmail.com>
2022-12-13 00:55:01 +0100

    add search-form

---
+++ src/App/components/search-form/SearchForm.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useActions, useValues } from 'kea'
+import { Button, Input, Select } from 'antd'
+
+import { appLogic } from '../../appLogic'
+
+export function SearchForm() {
+  const { setCity, setTemperatureUnit, fetchWeatherData } = useActions(appLogic)
+  const { city, temperatureUnit } = useValues(appLogic)
+
+  return (
+    <div className="search-form">
+      <Input
+        className="city-field"
+        value={city}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCity(e.target.value)}
+      />
+      <Select
+        className="temperature-unit-select"
+        defaultValue={temperatureUnit}
+        onChange={(value) => setTemperatureUnit(value)}
+        options={[
+          { value: 'celsius', label: 'Celsius' },
+          { value: 'fahrenheit', label: 'Fahrenheit' },
+        ]}
+      />
+      <Button className="submit-form-button" type="primary" onClick={fetchWeatherData}>
+        Submit
+      </Button>
+    </div>
+  )
+}
