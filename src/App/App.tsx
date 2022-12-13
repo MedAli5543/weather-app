784263b9da6141ee4832ee0170256af6d25a0443
MedAli5543 <ben.othmen5543@gmail.com>
2022-12-13 01:29:17 +0100

    show error message in fetch error

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,18 +1,34 @@
 import React from 'react'
-
+import { useValues } from 'kea'
+import { Result, Button } from 'antd'
 import { SearchForm, WeatherSlider, TemperatureGraph } from './components'
+
+import { appLogic } from './appLogic'
 
 import 'antd/dist/antd.css' // or 'antd/dist/antd.less'
 
 import './App.scss'
 
 export function App() {
+  const { error } = useValues(appLogic)
+
   return (
     <div className="App">
       <div className="container">
         <SearchForm />
-        <WeatherSlider />
-        <TemperatureGraph />
+        {error ? (
+          <Result
+            status="500"
+            title="500"
+            subTitle="Sorry, something went wrong."
+            extra={<Button type="primary">Back Home</Button>}
+          />
+        ) : (
+          <>
+            <WeatherSlider />
+            <TemperatureGraph />
+          </>
+        )}
       </div>
     </div>
   )
