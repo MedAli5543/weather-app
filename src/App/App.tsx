bf60672a83c82091b5c428c7478406e80d84265a
MedAli5543 <ben.othmen5543@gmail.com>
2022-12-13 01:16:38 +0100

    refactor app

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
+
+import { SearchForm, WeatherSlider, TemperatureGraph } from './components'
+
+import 'antd/dist/antd.css' // or 'antd/dist/antd.less'
+
 import './App.scss'
 
 export function App() {
-  return <div className="App">Hello</div>
+  return (
+    <div className="App">
+      <div className="container">
+        <SearchForm />
+        <WeatherSlider />
+        <TemperatureGraph />
+      </div>
+    </div>
+  )
 }
